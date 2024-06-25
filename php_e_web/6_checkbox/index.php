0bb124c12de432c22d2519e3685ff0ae84c39353
Geomis-Anonimo <130933017+Geomis-Anonimo@users.noreply.github.com>
2024-06-25 20:19:09 -0300

    Segundo commit

---
+++ php_e_web/6_checkbox/index.php
@@ -0,0 +1,39 @@
+<?php
+
+if(isset($_POST['ingredientes']))
+{
+  $ingredientes = $_POST['ingredientes'];
+  print_r($ingredientes);
+  
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="POST">
+    <div>
+      <input type="checkbox" name="ingredientes[]" value="Tomate"> Tomate
+    </div>
+    <div>
+      <input type="checkbox" name="ingredientes[]" value="Abóbora"> Abóbora
+    </div>
+    <div>
+      <input type="checkbox" name="ingredientes[]" value="Tomate"> Feijão
+    </div>
+    <div>
+      <input type="checkbox" name="ingredientes[]" value="Tomate"> Alface
+    </div>
+    <div>
+      <input type="checkbox" name="ingredientes[]" value="Tomate"> Cenoura
+    </div>
+    <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
